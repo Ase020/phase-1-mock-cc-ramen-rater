7e8c4d66aae6dfb9e897f033c8dfe39ccb658917
felixNyalenda <felixasejunior@gmail.com>
2023-03-29 11:21:57 +0300

    image build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {
+   const ramenMenu = document.getElementById("ramen-menu");
+   const ramenDetail = document.getElementById("ramen-detail");
+   const rating = document.getElementById("rating-display");
+   const comment = document.getElementById("comment-display");
+   fetch("http://localhost:3000/ramens")
+      .then((res) => res.json())
+      .then((res) => {
+         res.forEach((ramen) => {
+            const img = document.createElement("img");
+            img.src = ramen.image;
+            img.alt = ramen.name;
+            ramenMenu.append(img);
+
+            img.addEventListener("click", () => {
+               ramenDetail.innerHTML = `
+                <img class="detail-image" src=${ramen.image} alt="Insert Name Here" />
+    <h2 class="name">${ramen.name}</h2>
+    <h3 class="restaurant">${ramen.restaurant}</h3>
+                `;
+               comment.textContent = ramen.comment;
+               rating.textContent = ramen.rating;
+            });
+         });
+      });
+});
